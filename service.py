954f2db9465ee44a61c6a7eabc80fde07b4f9bc8
OCCCAS <pavlov.timur556@yandex.ru>
2021-11-03 15:13:49 +0300

    build: changed code style

--- service.py
+++ service.py
@@ -11,19 +11,19 @@ import os
 database_handler_ = DatabaseHandler(config.DATABASE_PATH)
 
 
-# Check is user created account
 def check_login_exists(data: dict) -> bool:
+    """Check is user created account"""
     return database_handler_.check_login_exists(data.get('email'), data.get('password'))
 
 
-# Creating file name for user avatar
 def create_local_avatar_file_name() -> str:
+    """Creating file name for user avatar"""
     user_email = hash_data(get_local_user_email())
     return f'user.{user_email}.avatar.png'
 
 
-# Creating file name for user portfolio photo
 def create_local_portfolio_file_name() -> str:
+    """Creating file name for user portfolio photo"""
     user_email = hash_data(get_local_user_email())
     photo_index = get_user_data_by_column('portfolio_count', user_email)
     photo_index = 0 if not photo_index else photo_index
@@ -31,8 +31,8 @@ def create_local_portfolio_file_name() -> str:
     return f'{user_email}_{photo_index}.png'
 
 
-# Create own user path
 def create_user_path() -> None:
+    """Create own user path"""
     user_email = hash_data(get_local_user_email())
     user_path = os.path.join(config.USERS_PATH, user_email)
     user_portfolio_path = os.path.join(user_path, 'portfolio')
@@ -42,8 +42,8 @@ def create_user_path() -> None:
         os.mkdir(user_portfolio_path)
 
 
-# Creating account
 def create_account(data: dict) -> bool:
+    """Creating account"""
     if not check_login_exists(data):
         save_current_user_to_local(data)
         create_user_path()
@@ -56,20 +56,20 @@ def create_account(data: dict) -> bool:
         return False
 
 
-# Get user info from column
 def get_user_data_by_column(column: str, email: str) -> Union[int, float, str, bool]:
+    """Get user info from column"""
     data = database_handler_.get_user_data_by_column(column, email)
     return data if data else False
 
 
-# Get user info from columns
 def get_user_data_by_columns(columns: Union[list, tuple], email: str) -> Union[int, float, str, bool]:
+    """Get user info from columns"""
     data = database_handler_.get_user_data_by_columns(columns, email)
     return data if data else False
 
 
-# Check is user logged in in his computer
 def is_user_logged_in_local() -> bool:
+    """Check is user logged in in his computer"""
     with open('user.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
         file.close()
@@ -81,8 +81,8 @@ def is_user_logged_in_local() -> bool:
     return False
 
 
-# Get user email which logged in in this computer (local)
 def get_local_user_email() -> Union[str, bool]:
+    """Get user email which logged in in this computer (local)"""
     with open('user.json', 'r', encoding='utf-8') as json_:
         json_data = json.load(json_)
         json_.close()
@@ -93,8 +93,8 @@ def get_local_user_email() -> Union[str, bool]:
     return False
 
 
-# Copy user avatar photo to application local path
 def copy_avatar_photo_to_local(file_name: str) -> str:
+    """Copy user avatar photo to application local path"""
     user_email = hash_data(get_local_user_email())
     new_file_name = create_local_avatar_file_name()
     new_file_path = os.path.join(os.path.join(config.USERS_PATH, user_email), new_file_name)
@@ -104,8 +104,8 @@ def copy_avatar_photo_to_local(file_name: str) -> str:
     return new_file_path
 
 
-# Copy user portfolio photo to application local path
 def copy_portfolio_photo_to_local(file_name: str) -> str:
+    """Copy user portfolio photo to application local path"""
     user_email = hash_data(get_local_user_email())
     user_portfolio_path = os.path.join(os.path.join(config.USERS_PATH, user_email), 'portfolio')
 
@@ -117,8 +117,8 @@ def copy_portfolio_photo_to_local(file_name: str) -> str:
     return new_file_path
 
 
-# Save in local, that user is logged in
 def save_current_user_to_local(data: dict) -> None:
+    """Save in local, that user is logged in"""
     with open('user.json', 'w', encoding='utf-8') as json_file:
         json.dump(
             {
@@ -129,93 +129,99 @@ def save_current_user_to_local(data: dict) -> None:
         json_file.close()
 
 
-# Edit profile
 def edit_profile(name: str, surname: str, gender: str, birthday: int, photo: str) -> None:
+    """Edit profile"""
     user_email = get_local_user_email()
-    photo = copy_avatar_photo_to_local(photo)
+    if photo:
+        photo = copy_avatar_photo_to_local(photo)
+    else:
+        photo = get_user_avatar_photo()
 
     database_handler_.edit_user_data_by_columns(['name', 'surname', 'birthday', 'gender', 'avatar_photo'],
                                                 [name, surname, birthday, gender, photo],
                                                 user_email)
 
 
-# Increase portfolio photo count (in database)
 def increase_photos_count() -> None:
+    """Increase portfolio photo count (in database)"""
     user_email = get_local_user_email()
     database_handler_.increase_portfolio_photo_count(user_email)
 
 
-# Add portfolio
 def add_to_portfolio(competitions_name: str, place: str, date: int, photo: str) -> bool:
+    """Add portfolio"""
     user_email = get_local_user_email()
     photo = copy_portfolio_photo_to_local(photo)
     increase_photos_count()
-
     database_handler_.add_portfolio_to_portfolios(user_email, competitions_name, place, date, photo)
     return True
 
 
-# Get user portfolio
 def get_user_portfolio() -> Union[list, tuple]:
+    """Get user portfolio"""
     user_email = get_local_user_email()
     return database_handler_.get_portfolio(user_email)
 
 
-# Get path to user avatar
 def get_user_avatar_photo() -> str:
+    """Get path to user avatar"""
     user_email = get_local_user_email()
     return get_user_data_by_column('avatar_photo', user_email)
 
 
-# Get full user data
 def get_full_user_data() -> Union[list, tuple]:
+    """Get full user data"""
     user_email = get_local_user_email()
     return database_handler_.get_full_user_data(user_email)
 
 
-# Check login data correctness
-def check_login_data_correctness(login_data: dict) -> bool: return database_handler_.check_login_data_correctness(login_data)
+def check_login_data_correctness(login_data: dict) -> bool:
+    """Check login data correctness"""
+    return database_handler_.check_login_data_correctness(login_data)
 
 
-# Read program messages file, and return message
-def get_filing_error_text(error_name: str) -> str:
+def get_static_interface_texts(name: str) -> str:
+    """Read program messages file, and return messages"""
     with open('program_messages.json', 'r', encoding='utf-8') as json_file:
         errors_message = json.load(json_file)
         json_file.close()
 
-    if error_name in errors_message['errors_text']:
-        return errors_message['errors_text'].get(error_name)
+    if name in errors_message['texts']:
+        return errors_message['texts'].get(name)
     else:
         return ''
 
 
-# Load task to database
 def create_task(title: str, text: str, date: int, photo: Union[str, None]) -> bool:
+    """Load task to database"""
     database_handler_.create_task(title, text, date, photo)
     return True
 
 
-# Get tasks
 def get_tasks() -> List[Tuple]:
+    """Get tasks"""
     return database_handler_.get_tasks()
 
 
-# Get user post
 def get_user_post_id() -> str:
+    """Get user post"""
     user_email = get_local_user_email()
     return get_user_data_by_column('post', user_email)
 
 
-# Get post id from table, because post column is foreign key (table: posts)
-def get_post_id(post_name: str) -> int: return database_handler_.get_post_id(post_name)
+def get_post_id(post_name: str) -> int:
+    """Get post id from table, because post column is foreign key (table: posts)"""
+    return database_handler_.get_post_id(post_name)
 
 
-# Get list of children (if post = 2)
-def get_children_list() -> list: return database_handler_.get_children_list()
+def get_children_list() -> list:
+    """Get list of children (if post = 2)"""
+    return database_handler_.get_children_list()
 
 
 def create_class(children_email: List[str]) -> bool:
+    """Create children class -> (insert or replace classes database and
+    set class columns at children)"""
     user_email = get_local_user_email()
     database_handler_.create_class(user_email, children_email)
     return True
-
